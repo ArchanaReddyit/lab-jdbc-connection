4bfbc7a1a505f1107caeb52e8dc132bf3fea903c
ArchanaReddyit <archanareddy.it@gmail.com>
2020-08-17 18:04:55 +0400

    Completed

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -4,10 +4,22 @@ import java.sql.Connection;
 
 import utility.ConnectionManager;
 
+
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
+		Connection con;
+		con=ConnectionManager.getConnection();
 		
-	// Fill your code
-		
-	}
+		if(con!=null) {
+			System.out.println("Connection Established");
+		}
+		else
+		{
+			System.out.println("Check your connection");
+		}	
+	
 }
+}
+
+
+	
